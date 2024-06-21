71fb68a6e30a1f26a9d7728a7de6cf3767ce1025
Prince Kaushal <95558284+Prince9670@users.noreply.github.com>
2024-06-21 16:25:48 +0530

    first commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
 const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
-const API_KEY = "YOUR-API-KEY";
+const API_KEY = "002e9362767097c5390ce5615d31d6e4";
 export const endpointPath = (country, category) =>
   `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
 export const endpointSearch = (searchQuery) =>
